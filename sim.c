d50f9324aeeaf0959b1fea89c52562325d155840
mirandaalex <amiranda.5_1997@hotmail.com>
2020-05-20 05:28:37 -0500

    Se agrego dinamica

--- sim.c
+++ sim.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int funEstatica(void){
 	int memoria=1;
@@ -49,14 +50,110 @@ int funEstatica(void){
 			}	
 		}
 	}
+	printf("Error al reservar memoria\n");
 	return 0;
 }
-
+void imprimeDinamica(float **memo, bool **part,int count){
+	printf("         Memoria | Disponibilidad\n" );
+	for (int i = 0; i <= count; ++i)
+	{
+		//printf("         Memoria | Disponibilidad\n" );
+		printf("[ _ _ ]    %2.2f |  %s \n",memo[0][i],part[0][i]==false? "Disponible":"Ocupada");
+	}
+	printf("\n-----------------------------------------------------------------\n\n");		
+	printf("Precione enter para continuar\n");
+	getchar();
+	getchar();
+					
+	
+}
 
 int funDinamica(void){
+	int memoriatotal=1,particiones=0;
+	printf("Ingrese la memoria deseada en Mb\n");
+	scanf("%d",&memoriatotal);
+	printf("\nMemoria %d Mb \n",memoriatotal);
+	bool *part;
+	float *memo;
+	memo = (float *)calloc(1,sizeof(float));
+	part = (bool *)calloc(1,sizeof(bool));
+	if (part!=NULL && memo!=NULL){
+		int op,auxi;
+		float esp;
+		memo[0]=(float)memoriatotal;
+		part[0]=false;
+		printf("\n\nMemoria total:  %.2f Mb\n",memo[0] );
+		imprimeDinamica(&memo,&part,particiones);
+		while(1){
+			printf("\n-----------------------------------------------------------------\n\n");
+			printf("\tParticion Dinamica\n");
+			printf("\t1 Agreagar proceso\n");
+			printf("\t2 Eliminar proceso\n");
+			printf("\t3 Ver memoria\n");
+			printf("\t4 Salir al menu\n");
+			printf("\n-----------------------------------------------------------------\n\n");
+			printf("ingresa la opcion\n");
+			scanf("%d",&op);
+			switch(op){
+				case 1:
+					printf("Ingresa el espacio ocupado del proceso en Mb\n");
+					scanf("%f",&esp);
+					int i = 0;
+					float aux;
+					bool temp=false;
+					for (i=0; i <=particiones; ++i){
+						if (memo[i]==esp && part[i]==false && temp==false){
+							part[i]=true;
+							temp=true;
+							imprimeDinamica(&memo,&part,particiones);
+		
+						}else if(memo[i]>esp && part[i]==false && temp==false){				
+							memo=(float *)realloc(memo,sizeof(float)*(particiones+2));
+							part=(bool *)realloc(part,sizeof(bool)*(particiones+2));
+							if (part!=NULL && memo!=NULL){
+								aux=memo[i]-esp;
+								memo[i]=esp;
+								part[i]=true;
+								temp=true;
+								++particiones;
+								memo[particiones]=aux;
+								part[particiones]=false;
+								imprimeDinamica(&memo,&part,particiones);
+							}else{
+								printf("Error al usar malloc");
+								break;
+							}
+						}else if(memo[i]<esp && i==particiones){
+							printf("No hay memoria suficiente\n");
+						}
+					}
+					break;
+				case 2:
+					printf("Ingresa el proceso que deseas matar\n");
+					scanf("%d",&auxi);
+					if (auxi>=0 && auxi<=particiones ){
+						part[auxi]!=false? part[auxi]=false:printf("El proceso no se encuentra ocupado\n");;
+					}else printf("No existe el proceso: %d\n",auxi);
+					imprimeDinamica(&memo,&part,particiones);
+					break;
+				case 3:
+					imprimeDinamica(&memo,&part,particiones);
+					break;
+				case 4:
+					free(part);
+					free(memo);
+					return 1;
+				default:
+					printf("\nOpcion no valida\n");
+					printf("Precione enter para continuar\n");
+					getchar();
+					getchar();
 
+			}	
 
-
+		}
+	}
+	printf("Error al reservar memoria\n");
 	return 0;
 }
 
